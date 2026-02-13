807cf7989776e7ae6431f44f31f7bb62d82b0129
Riegardt Weich <riegardt.weich@gmail.com>
2026-02-13 15:47:18 +0200

    Drone Spawns And TP

--- 2d-boid-nav/Scenes/drone.gd
+++ 2d-boid-nav/Scenes/drone.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
+var random_dir
+var movement_speed = 150
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var random_angle = randf_range(0, 2 * PI)
-	var random_dir = Vector2(cos(random_angle), sin(random_angle)).normalized()
+	random_dir = Vector2(cos(random_angle), sin(random_angle)).normalized()
 	rotation = random_angle
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +13,20 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	var dir = random_dir
+	linear_velocity = dir*movement_speed
+	#apply_central_force(random_dir*movement_speed)
 	#add physics to make the drone actually move in a random dir 
 	#then add a box, in which they teleport to the other side once a edge is hit
-	
+	if self.position.x < 0: 
+		global_position.x = 1200
+		linear_velocity = dir*movement_speed
+	if self.position.x > 1200: 
+		global_position.x = 0
+		linear_velocity = dir*movement_speed
+	if self.position.y < 0: 
+		global_position.y = 600
+		linear_velocity = dir*movement_speed
+	if self.position.y > 600: 
+		global_position.y = 0
+		linear_velocity = dir*movement_speed
---
+++ 2d-boid-nav/Scenes/drone_spawner.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var drone_scene: PackedScene = preload("res://Scenes/drone.tscn")
+@export var spawn_count: int = 50  # Number of enemies per wave
+@export var spawn_area: Rect2 = Rect2(0, 0, 1000, 1000)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_wave()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_wave():
+	for i in range(spawn_count):
+		spawn_drone()
+
+func spawn_drone():
+	var drone = drone_scene.instantiate()
+	var spawn_pos = Vector2(
+		randf_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x),
+		randf_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y)
+	)
+	drone.global_position = spawn_pos
+	add_child(drone)
