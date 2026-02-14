588bcff15c8bfe7e79fb72c291a5ce86e363b73f
Riegardt Weich <riegardt.weich@gmail.com>
2026-02-14 11:57:41 +0200

    Beginning Of Boid Steering
    Not there yet but it works

--- 2d-boid-nav/Scenes/drone.gd
+++ 2d-boid-nav/Scenes/drone.gd
@@ -3,53 +3,69 @@ extends RigidBody2D
 enum Drone{}
 
 var random_dir
+var dir
 var movement_speed = 150
 var drones_in_range: Array[Node2D] = []
+
+@export var Steer_Strength = 0.05
+@export var Seperation = 1
+@export var Cohesion = 1
+@export var Alignment = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var random_angle = randf_range(0, 2 * PI)
 	random_dir = Vector2(cos(random_angle), sin(random_angle)).normalized()
 	rotation = random_angle
+	dir = random_dir
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	var dir = random_dir
-	linear_velocity = dir*movement_speed
-	#apply_central_force(random_dir*movement_speed)
-	#add physics to make the drone actually move in a random dir 
-	#then add a box, in which they teleport to the other side once a edge is hit
+	dir += _Steer_Apply()*Steer_Strength
+	rotation = dir.angle()
+	
 	if self.position.x < 0: 
 		global_position.x = 1200
-		linear_velocity = dir*movement_speed
 	if self.position.x > 1200: 
 		global_position.x = 0
-		linear_velocity = dir*movement_speed
 	if self.position.y < 0: 
 		global_position.y = 600
-		linear_velocity = dir*movement_speed
 	if self.position.y > 600: 
 		global_position.y = 0
-		linear_velocity = dir*movement_speed
+	
+	#if linear_velocity.length() < 50:
+		#linear_velocity += dir*movement_speed
+	linear_velocity = dir.normalized()*movement_speed
+
+
+func _Steer_Apply() -> Vector2:
+	var direction = Vector2.ZERO
+	direction =+ Seperation * _Steer_Seperation()
+	+ Alignment * _Steer_Alignment()
+	+ Cohesion * _Steer_Cohesion()
+	return direction.normalized()
 
 func _Steer_Seperation() -> Vector2:
 	var direction = Vector2.ZERO
-	return direction
+	for drone in drones_in_range:
+		var ratio = 1
+		direction -= ratio * (drone.global_position - global_position)
+	return direction.normalized()
 
 func _Steer_Cohesion() -> Vector2:
 	var direction = Vector2.ZERO
 	return direction
 
-func _Steer_Center() -> Vector2:
+func _Steer_Alignment() -> Vector2:
 	var direction = Vector2.ZERO
 	return direction
 
 func _on_boid_area_area_entered(body : Area2D) -> void:
 	var drone = body.get_parent()
-	print('Im in danger')
 	if not drone.is_in_group("Drones"): 
 		return
 	
@@ -60,7 +76,7 @@ func _on_boid_area_area_entered(body : Area2D) -> void:
 		return
 	
 	drones_in_range.append(drone)
-	print(name, " sees: ", drone.name, " at ", drone.global_position)
+	#print(name, " sees: ", drone.name, " at ", drone.global_position)
 	
 	pass # Replace with function body.
 
@@ -69,5 +85,5 @@ func _on_boid_area_area_exited(body : Area2D) -> void:
 	var drone = body.get_parent()
 	if drones_in_range.has(drone):
 		drones_in_range.erase(drone)
-		print(name, " lost sight of: ", drone.name)
+		#print(name, " lost sight of: ", drone.name)
 	pass # Replace with function body.
